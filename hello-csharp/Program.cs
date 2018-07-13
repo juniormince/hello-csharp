ee77514ed20e21a4ae56bdd4bf2578b194548765
juniormince <groundcontroltomajorbree@gmail.com>
2018-07-13 08:54:10 -0500

    add initial files

---
+++ hello-csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
